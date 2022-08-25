8d7585064d813b92aa4ba39ed53fd0b403bc2ced
connorlicht <111907619+connorlicht@users.noreply.github.com>
2022-08-25 18:37:27 +0000

    initial commit

---
+++ hello.c
@@ -0,0 +1,17 @@
+/**
+ * Author: Connor Licht
+ *         connorlicht@unomaha.edu
+ * Date: 2022/08/23
+ *
+ * A simple hello world program in C
+ *
+ */
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(int argc, char **argv) {
+
+  printf("Connor Licht\n");
+
+  return 0;
+}
